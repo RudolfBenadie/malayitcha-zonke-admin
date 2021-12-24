809cb1ab7af083a62d68892f189f1ad3750c75ef
Rudolf Benadie <benadier@leadforensics.com>
2021-12-24 08:34:41 +0000

    Add logo

--- src/layout/navBar.jsx
+++ src/layout/navBar.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Navbar } from "reactstrap";
 import AuthStatus from "../widgets/authStatus";
+import logo from '../assets/images/LogoTransparent50.png';
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
   return (
     <Navbar>
       <div className="nav-wrapper">
-        <div className="nav-section">
+        <div className="nav-section nav-brand" onClick={() => navigate("/")}>
+          <img src={logo} alt="Malayitcha logo - truck in motion" />
           <span>Malayitcha Zonke</span>
         </div>
-        <div className="nav-section-flex-3">
+        <div className="nav-section nav-menu">
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -22,7 +25,7 @@ const NavigationBar = () => {
             </li>
           </ul>
         </div>
-        <div className="nav-section flex-justify-right">
+        <div className="nav-section nav-authstatus">
           <AuthStatus />
         </div>
       </div>
